c93a1af3f543ec415a3bf66076ed4f6d57ee277d
bfollek <bfollek@gmail.com>
2023-08-31 12:45:39 -0400

    Work In Progress

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,11 @@
 //! tests/health_check.rs
+//!
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
