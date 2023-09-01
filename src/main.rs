d6a611f2d276cef364f40cc4ac492774123eb065
bfollek <bfollek@gmail.com>
2023-08-31 21:58:01 -0400

    Back to port 8000 for main.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("localhost:0")?;
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    print!("\n\t\tRunning on http://localhost:{}\n\n", port);
+    let listener = TcpListener::bind("localhost:8000")?;
     run(listener)?.await
 }
