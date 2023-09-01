39447d01b2c36012892e9f15f2c0699896ae3fba
bfollek <bfollek@gmail.com>
2023-08-31 21:30:54 -0400

    Use localhost instead of 127.0.0.1.

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 ### Commands
 
-* curl -v http://127.0.0.1:8000/health_check
 * Start a file watcher: `cargo watch -x check -x test -x run`
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let listener = TcpListener::bind("localhost:0").expect("Failed to bind random port");
     let _ = listener.local_addr().unwrap().port();
     run(listener)?.await
 }
