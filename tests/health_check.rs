112b4b094591bbe78d454bd8b5e0db270495869d
bfollek <bfollek@gmail.com>
2023-08-31 21:29:07 -0400

    Get open port from OS.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,46 +1,31 @@
 //! tests/health_check.rs
 //!
-// `tokio::test` is the testing equivalent of `tokio::main`.
-// It also spares you from having to specify the `#[test]` attribute.
-//
-// You can inspect what code gets generated using
-// `cargo expand --test health_check` (<- name of the test file)
+
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    //  Arrange
-    // No .await, no .expect
-    spawn_app();
+    // Arrange
+    let address = spawn_app();
     let client = reqwest::Client::new();
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
-
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-// No .await call, therefore no need for `spawn_app` to be async now.
-// We are also running tests, so it is not worth it to propagate errors:
-// if we fail to perform the required setup we can just panic and crash
-// all the things.
-fn spawn_app() {
-    // "Port 0 is special-cased at the OS level: trying to bind port 0 will trigger
-    // an OS scan for an available port which will then be bound to the application."
-    let server = zero2prod::run("localhost:0").expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-
-    // "A second look at tokio::spawn’s documentation supports our hypothesis:
-    // when a tokio runtime is shut down all tasks spawned on it are dropped.
-    // tokio::test spins up a new runtime at the beginning of each test case and
-    // they shut down at the end of each test case. In other words, good news -
-    // no need to implement any clean up logic to avoid leaking resources between test
-    // runs." - p. 36
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
